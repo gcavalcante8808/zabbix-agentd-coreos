5568e31f407c52ed9f0e3a88aafa63a55ca79e92
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2016-12-08 18:25:53 -0200

    Added config bootstrap script to the agentd bs script.

--- scripts/bootstrap_agentd.sh
+++ scripts/bootstrap_agentd.sh
@@ -5,7 +5,41 @@ echo "Downloading Zabbix Agentd ${ZBX_VERSION}"
 wget https://github.com/gcavalcante8808/zabbix-agentd-coreos/releases/download/{$ZBX_VERSION}/zabbix_agentd -O /opt/bin/zabbix_agentd && \
 chmod +x /opt/bin/zabbix_agentd
 
-echo "Download Configuration BootStrap Script"
-wget https://raw.githubusercontent.com/gcavalcante8808/docker-zabbix-agent/master/files/docker-entrypoint.sh -O /tmp/bootstrap.sh && \
-chmod +x /tmp/bootstrap.sh
+echo "Starting Config Generation"
+CONF="/opt/local/etc/zabbix_agentd.conf.d"
+
+if [ -z ${ZABBIX_SERVER} ]; then
+    echo "No default zabbix-server provided. Exiting ..."
+    exit 1
+fi
+
+if [ -z ${REMOTE_COMMAND} ]; then
+    echo "No Remote Command Configuration Provided. Assuming 0 (no support)"
+    REMOTE_COMMAND=0
+fi
+
+if [ -z ${ZABBIX_SERVER_PORT} ];then
+    echo "No Zabbix Server Port Provided. Assuming 10051."
+    ZABBIX_SERVER_PORT=10051
+fi
+
+if [ ! -d "/tmp/firstrun" ]; then
+
+mkdir -p $CONF
+
+cat <<EOT > $CONF/general.conf
+LogFile=/tmp/zabbix_server.log
+LogFileSize=1024
+LogType=console
+ListenPort=10050
+DebugLevel=3
+EnableRemoteCommands=${REMOTE_COMMAND}
+LogRemoteCommands=1
+Server=${ZABBIX_SERVER}
+ServerActive=${ZABBIX_SERVER}:${ZABBIX_SERVER_PORT}
+Timeout=30
+EOT
+
+    touch /tmp/firstrun
+fi
 
