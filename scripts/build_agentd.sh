7e201203817d51789939cf6c1dabce85474beb72
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2016-12-09 09:03:07 -0200

    There is no need to build the agent for now.

--- scripts/build_agentd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-cd /usr/local/src && \
-   wget http://ufpr.dl.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/${ZBX_VERSION}/zabbix-${ZBX_VERSION}.tar.gz && \
-   tar xf zabbix-${ZBX_VERSION}.tar.gz && \
-   cd /usr/src/zabbix-${ZBX_VERSION} && \
-   ./configure --enable-agent --enable-static && \
-   make -j && make install
